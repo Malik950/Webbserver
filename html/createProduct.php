8c8f01cd6b08a0d7696c3c29f8222a6f0193e678
Malik950 <54435682+Malik950@users.noreply.github.com>
2021-03-09 14:36:57 +0100

    Add files via upload

--- html/createProduct.php
+++ html/createProduct.php
@@ -1,23 +1,8 @@
 <?php
-$target_dir = "bilder/";
-$target_file = $target_dir . basename($_FILES["picture"]["name"]);
-$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+header("Content-type:text/html; charset = utf-8");
+session_start();
+$h1span = "Update";
+require "../template/createProduct-template.php";
 
-$check = getimages($_FILES["picture"]["tmp_name"]);
-if($check === false) {
-header("location:createProduct.php?errmsg=1");
-}
-
-if(file_exists($target_file)) {
-header("location:createproduct.php?errmsg=2");
-}
-
-if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "gif") {
-header("location.createProduct.php?errmsg=3");
-}
-
-if(move_uploaded_file($_FILES["picture"]["tmp_name"], $target_file)) {
-$sql = "INSRET INTO products(name, description, price, picture) VALUE(?,?,?,?)";
-$res=$dbh->prepare($sql);
-$res->bind_param("ssis",$namn, $descr, $pris, $target_file);
-}
+?>
+    
